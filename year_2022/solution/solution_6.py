e6a12baca759a3c05d42c4a50ca3accf1c9e7ded
Zynko <zynko1234@gmail.com>
2022-12-06 06:36:46 -0500

    FIXUP added solutions 6 for 2022

---
+++ year_2022/solution/solution_6.py
@@ -0,0 +1,67 @@
+import copy
+from re import L
+
+def solve(in_list):
+    ansA = None
+    ansB = None
+
+    data = normalize_input(in_list)
+
+    ansA = partA(data)
+    ansB = partB(data)
+    return ansA, ansB
+
+def partA(data):
+    ret = 0
+    width = 4
+    buffer = []
+
+    for glyph in data:
+
+        if len(buffer) == width:
+            tmp_set = set(buffer)
+
+            # Check for 4 unique characters.
+            if len(tmp_set) == width:
+                break
+            else:
+                buffer = buffer[1:]
+                buffer.append(glyph)
+
+        else:
+            buffer.append(glyph)
+
+        ret += 1
+
+    return ret
+
+def partB(data):
+    ret = 0
+    width = 14
+    buffer = []
+
+    for glyph in data:
+
+        if len(buffer) == width:
+            tmp_set = set(buffer)
+
+            # Check for 4 unique characters.
+            if len(tmp_set) == width:
+                break
+            else:
+                buffer = buffer[1:]
+                buffer.append(glyph)
+
+        else:
+            buffer.append(glyph)
+
+        ret += 1
+
+    return ret
+
+
+def normalize_input(input):
+    ret = input[0]
+
+    return ret
+
